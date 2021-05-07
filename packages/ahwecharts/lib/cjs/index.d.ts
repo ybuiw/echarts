6a5c95c5ee89cbb6123d9d6ced586ec6e72e3dc4
姚宾 <yaob421123@163.com>
2021-05-07 17:55:32 +0800

    edit ahwecharts

--- packages/ahwecharts/lib/cjs/index.d.ts
+++ packages/ahwecharts/lib/cjs/index.d.ts
@@ -1,8 +1,6 @@
 export * from '@ahwecharts/bar-base';
 export * from '@ahwecharts/line-base';
-export * from '@ahwecharts/line-stacked';
 export * from '@ahwecharts/pie-base';
 export { default as BarBaseEcharts } from '@ahwecharts/bar-base';
 export { default as LineBaseEcharts } from '@ahwecharts/line-base';
-export { default as LineStackedEcharts } from '@ahwecharts/line-stacked';
 export { default as PieBaseEcharts } from '@ahwecharts/pie-base';
--- packages/ahwecharts/src/index.ts
+++ packages/ahwecharts/src/index.ts
@@ -1,9 +1,9 @@
 export * from '@ahwecharts/bar-base';
 export * from '@ahwecharts/line-base';
-export * from '@ahwecharts/line-stacked';
+// export * from '@ahwecharts/line-stacked';
 export * from '@ahwecharts/pie-base';
 
 export { default as BarBaseEcharts } from '@ahwecharts/bar-base';
 export { default as LineBaseEcharts } from '@ahwecharts/line-base';
-export { default as LineStackedEcharts } from '@ahwecharts/line-stacked';
+// export { default as LineStackedEcharts } from '@ahwecharts/line-stacked';
 export { default as PieBaseEcharts } from '@ahwecharts/pie-base';
